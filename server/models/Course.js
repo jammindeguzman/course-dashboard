c5aaf4cca568e2f35a28d23f9a509e46a5b1a863
jammindeguzman <jammindeguzman@gmail.com>
2025-07-25 16:34:08 +0800

    Delete server directory

--- server/models/Course.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const courseSchema = new mongoose.Schema({
-  _id: String,
-  title: String,
-  description: String,
-});
-
-export default mongoose.model('Course', courseSchema);
